158c9c03e9e9c21cc44a4510614557d71332ffc1
黃信慈 <dei8546@gmail.com>
2017-05-02 14:16:50 +0800

    補上傳

--- Firebsetest/app/src/main/java/com/example/user/firebsetest/ProfileActivity.java
+++ Firebsetest/app/src/main/java/com/example/user/firebsetest/ProfileActivity.java
@@ -1,47 +1,78 @@
 package com.example.user.firebsetest;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+    import android.content.Intent;
+    import android.support.v7.app.AppCompatActivity;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+    import com.google.firebase.auth.FirebaseAuth;
+    import com.google.firebase.auth.FirebaseUser;
+    import com.google.firebase.database.DatabaseReference;
+    import com.google.firebase.database.FirebaseDatabase;
 
-public class ProfileActivity extends AppCompatActivity {
+    public class ProfileActivity extends AppCompatActivity {
 
-    private FirebaseAuth firebaseAuth;
+        private FirebaseAuth firebaseAuth;
 
-        private TextView UserEmail;
-        private Button buttonLogout;
+    private TextView UserEmail;
+    private Button buttonLogout;
+    private DatabaseReference databaseReference;
+    private EditText editTextUserName,editTextUserNumber;
+    private Button buttonEnter;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_profile);
 
-            firebaseAuth=FirebaseAuth.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        firebaseAuth = FirebaseAuth.getInstance();
         if (firebaseAuth.getCurrentUser() == null) {
             //profile activity here
-            startActivity(new Intent(ProfileActivity.this,loginActivity.class));
+            startActivity(new Intent(ProfileActivity.this, loginActivity.class));
             ProfileActivity.this.finish();
         }
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        editTextUserName = (EditText) findViewById(R.id.UserName);
+        editTextUserNumber = (EditText) findViewById(R.id.UserNumber);
+        buttonEnter = (Button) findViewById(R.id.buttonenter);
+        buttonLogout = (Button) findViewById(R.id.buttonlogout);
 
-        FirebaseUser user =firebaseAuth.getCurrentUser();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
 
-        UserEmail=(TextView)findViewById(R.id.textViewUserEmail);
-        buttonLogout=(Button)findViewById(R.id.buttonlogout);
-        UserEmail.setText("歡迎"+user.getEmail()+"加入!");
+        buttonEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserInformation();
+            }
+        });
         buttonLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 firebaseAuth.signOut();
 
-                startActivity(new Intent(ProfileActivity.this,loginActivity.class));
+                startActivity(new Intent(ProfileActivity.this, loginActivity.class));
                 ProfileActivity.this.finish();
             }
         });
+
+        UserEmail.setText("歡迎" + user.getEmail() + "加入!");
     }
+    private void saveUserInformation() {
+        String name = editTextUserName.getText().toString().trim();
+        String number = editTextUserNumber.getText().toString().trim();
+
+        UserInformation userInformation = new UserInformation(name, number);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        databaseReference.child(user.getUid()).setValue(userInformation);
+
+        Toast.makeText(this, "儲存資料中...", Toast.LENGTH_LONG).show();
+    }
+
 }
---
+++ Firebsetest/app/src/main/java/com/example/user/firebsetest/UserInformation.java
@@ -0,0 +1,16 @@
+package com.example.user.firebsetest;
+
+/**
+ * Created by WennLAB on 2017/4/25.
+ */
+
+public class UserInformation {
+    public String name;
+    public String number;
+
+
+    public UserInformation(String name,String number){
+        /*this.name;
+        this.number;*/
+    }
+}
